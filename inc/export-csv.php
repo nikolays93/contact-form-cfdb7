13d49c6ab2e97002c026163e0af133e92233aa97
Nikolays93 <Nikolays93@ya.ru>
2017-09-26 00:06:14 +0400

    add export del filter

--- inc/export-csv.php
+++ inc/export-csv.php
@@ -48,10 +48,10 @@ class Expoert_CSV{
             $tmp = str_replace($unwanted, '', $aKeys);
             $heading[] = ucfirst($tmp);
         }
-        fputcsv($df, $heading);
+        fputcsv($df, $heading, apply_filters( 'CFDB7_CSV_Export_delimiter', ',' ) );
 
         foreach ($array as $row) {
-            fputcsv($df, $row);
+            fputcsv($df, $row, apply_filters( 'CFDB7_CSV_Export_delimiter', ',' ) );
         }
         fclose($df);
         return ob_get_clean();
